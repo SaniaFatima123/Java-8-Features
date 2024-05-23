6d0ac1cc2dc540603c27fde68d08851faecee2d3
SaniaFatima123 <fatimasania648@gmail.com>
2024-05-23 14:10:14 +0530

    method reference added

---
+++ lambda expression/lambdaExpressions/src/main/java/org/example/Compare.java
@@ -0,0 +1,63 @@
+package org.example;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee(){}
+    public Employee(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+public class Compare  {
+
+    public static void main(String[] args){
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(new Employee("Shrey", 24));
+        employees.add(new Employee("Suhana", 23));
+        employees.add(new Employee("Priti", 21));
+        employees.add(new Employee("Naina", 20));
+        employees.add(new Employee("Ananya", 25));
+
+        // using static method reference to sort arraylist by name **********
+        Collections.sort(employees, Compare::compareByName);
+
+        // printing all the name using stream after name comparision
+        employees.stream().map(x -> x.getName()).forEach(System.out::println);
+
+//        // using static method reference to sort arraylist by age **********
+//        Collections.sort(employees, Compare::compareByAge);
+//
+//        // printing all the name using stream after age comparision
+//        employees.stream().map(x -> x.getAge()).forEach(System.out::println);
+     }
+
+     public static int compareByName(Employee a, Employee b){
+        return a.getName().compareTo(b.getName());
+     }
+//     public static int compareByAge(Employee a, Employee b){
+//        return a.getAge().compareTo(b.getAge());
+//     }
+
+}
--- lambda expression/lambdaExpressions/src/main/java/org/example/StudentService.java
+++ lambda expression/lambdaExpressions/src/main/java/org/example/StudentService.java
@@ -6,11 +6,12 @@ import java.util.function.Predicate;
 public class StudentService {
     ArrayList<Student> students = new ArrayList<>();
     public StudentService(){
-        students.add(new Student("Riya Saha", 23, 5, 2000));
-        students.add(new Student("Suhana Sidd", 24, 5, 2000));
-        students.add(new Student("Prakash Mehta", 11, 6, 3000));
         students.add(new Student("Kirti Patel", 12, 6, 3000));
+        students.add(new Student("Riya Saha", 23, 5, 2000));
+        students.add(new Student("Prakash Mehta", 11, 6, 3000));
+        students.add(new Student("Suhana Sidd", 24, 5, 2000));
         students.add(new Student("Pratik Pandey", 13, 6, 3000));
+        students.add(new Student("Raina Khan", 15, 7, 4000));
     }
     public ArrayList<Student> allStudents(){
         ArrayList<Student> allStudents = new ArrayList<>();
